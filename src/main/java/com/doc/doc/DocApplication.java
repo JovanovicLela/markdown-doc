e545feebf509fe983387a6e57d1a291473ecefa3
JovanovicLela <jovanovic.lela.00@gmail.com>
2023-03-12 12:54:36 +0100

    test commit 2

--- src/main/java/com/doc/doc/DocApplication.java
+++ src/main/java/com/doc/doc/DocApplication.java
@@ -9,6 +9,7 @@ public class DocApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DocApplication.class, args);
 		System.out.println("test commit");
+		System.out.println("test commit2");
 	}
 
 	// test commit
