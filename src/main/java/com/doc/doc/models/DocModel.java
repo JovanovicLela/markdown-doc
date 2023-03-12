a8b376009b11a0cc6558baffee31d0a8a00cfdee
JovanovicLela <jovanovic.lela.00@gmail.com>
2023-03-12 14:33:28 +0100

    document model init

---
+++ src/main/java/com/doc/doc/models/DocModel.java
@@ -0,0 +1,34 @@
+package com.doc.doc.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import static java.util.Objects.isNull;
+
+@Data
+@Document(collection = "docs")
+public class DocModel implements Serializable, Persistable<String> {
+
+    @Id
+    private String id;
+    private String content;
+    private String userId;
+    private String title;
+    private Boolean available;
+    @CreatedDate
+    private Date dateCreated;
+    @LastModifiedDate
+    private Date dateUpdated;
+
+    @Override
+    public boolean isNew() {
+        return isNull(this.dateCreated);
+    }
+}
