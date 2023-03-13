8d0942f46b7df1c7763f0075bc32100bbfe19421
JovanovicLela <jovanovic.lela.00@gmail.com>
2023-03-13 21:13:01 +0100

    added a cors filter

--- src/main/java/com/doc/doc/config/BeanConfig.java
+++ src/main/java/com/doc/doc/config/BeanConfig.java
@@ -3,6 +3,10 @@ package com.doc.doc.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class BeanConfig {
@@ -14,5 +18,22 @@ public class BeanConfig {
         modelMapper.getConfiguration().setSkipNullEnabled(true);
 
         return modelMapper;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.applyPermitDefaultValues();
+
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+        config.addAllowedMethod(HttpMethod.OPTIONS);
+
+        configSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(configSource);
     }
 }
